5d2535ae8c5d18f4626d192aa858565aa05655d7
zamesow <zamesow@gmail.com>
2020-12-29 13:57:07 +0200

    update files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'modern-normalize/modern-normalize.css';
+import './modern-normaize/modern-normalize.css'
 import App from './App';
 
 ReactDOM.render(
